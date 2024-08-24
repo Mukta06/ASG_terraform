89700468830e97da726c853ed2345e8c2af75eb5
Mukta06 <cbmukta06@gmail.com>
2024-08-25 01:30:20 +0530

    Auto push

--- tf-module-vpc/ec2.tf
+++ tf-module-vpc/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Instance" {
     ami             =data.aws_ami.ami.id
     instance_type   = "t2.micro"
     subnet_id       =aws_subnet.subnet.*.id
-    vpc_security_group_ids =aws_security_group.sg.id
+    vpc_security_group_ids =[aws_security_group.sg.id]
     
     tags = {
       Name= "Instance-${count.index}"
