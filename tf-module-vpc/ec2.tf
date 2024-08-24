1098db51171732c929df4b4e7c8cd60df192e511
Mukta06 <cbmukta06@gmail.com>
2024-08-25 01:59:51 +0530

    Auto push

--- tf-module-vpc/ec2.tf
+++ tf-module-vpc/ec2.tf
@@ -1,20 +1,21 @@
-# resource "aws_instance" "Instance" {
-#     count                    = length(aws_subnet.subnet.*.id)
-#     ami                      = data.aws_ami.ami.id
-#     instance_type            = "t2.micro"
-#     subnet_id                = element(aws_subnet.subnet.*.id, count.index)
-#     #subnet_id               =element(aws_subnet.subnet.*.id, count.index)
-#     vpc_security_group_ids   = [aws_security_group.sg.id]
-#     iam_instance_profile     = "b57-admin-iam-role" 
-#     user_data                = << EOF
+resource "aws_instance" "Instance" {
+    count                    = length(aws_subnet.subnet.*.id)
+    ami                      = data.aws_ami.ami.id
+    instance_type            = "t2.micro"
+    subnet_id                = element(aws_subnet.subnet.*.id, count.index)
+    #subnet_id               =element(aws_subnet.subnet.*.id, count.index)
+    vpc_security_group_ids   = [aws_security_group.sg.id]
+    iam_instance_profile     = "b57-admin-iam-role" 
+    # user_data                = <<EOF
 
-#         #!/bin/bash
-#         echo "Use User Data To Deploy Code "
-#     EOF 
+    #     #!/bin/bash
+    #     echo "Use User Data To Deploy Code "
+    # EOF 
+    #user_data= file(<filename.sh>)
     
-#     tags = {
-#       Name= "Instance-${count.index+1}"
-#     }
+    tags = {
+      Name= "Instance-${count.index+1}"
+    }
 
   
-# }
+}
