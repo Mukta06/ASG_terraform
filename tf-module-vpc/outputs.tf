477bc9401178c00c5fa1372e18331897fa17bf7b
Mukta06 <cbmukta06@gmail.com>
2024-08-25 21:08:34 +0530

    Auto push

--- tf-module-vpc/outputs.tf
+++ tf-module-vpc/outputs.tf
@@ -1,8 +1,8 @@
-output "elb_dns_name" {
-  value = "ELB URL is : ${aws_elb.ELB.dns_name}"
-}
+# output "elb_dns_name" {
+#   value = "ELB URL is : ${aws_elb.ELB.dns_name}"
+# }
 
 
-output "instance_ids" {
-  value = "Instance id's  : ${aws_instance.Instance.id}"
-}
+# output "instance_ids" {
+#   value = "Instance id's  : ${aws_instance.Instance.id}"
+# }
