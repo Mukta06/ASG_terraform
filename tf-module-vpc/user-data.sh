6d82fd851952ddb58e94e65e9bdf942cab26e59d
Mukta06 <cbmukta06@gmail.com>
2024-08-26 14:31:47 +0530

    Auto push

--- tf-module-vpc/user-data.sh
+++ tf-module-vpc/user-data.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+# #!/bin/bash
 
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "<h1>The Server IP is : $(hostname -i)</h1> " > /var/www/html/index.html
+# yum update -y
+# yum install -y httpd
+# systemctl start httpd
+# systemctl enable httpd
+# echo "<h1>The Server IP is : $(hostname -i)</h1> " > /var/www/html/index.html
