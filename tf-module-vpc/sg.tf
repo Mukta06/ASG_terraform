47e38b37a1490a60a36985f684c65c5e182bc895
Mukta06 <cbmukta06@gmail.com>
2024-08-26 14:08:43 +0530

    Auto push

--- tf-module-vpc/sg.tf
+++ tf-module-vpc/sg.tf
@@ -30,8 +30,8 @@ resource "aws_security_group" "ec2-sg" {
     vpc_id = aws_vpc.Mukta.id
 
     ingress {
-        from_port            = 0
-        to_port              = 0
+        from_port            = 80
+        to_port              = 80
         protocol             = "-1"
         security_groups      = [aws_security_group.alb-sg.id]
     }
