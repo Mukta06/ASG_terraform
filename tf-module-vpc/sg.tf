1522b4f312ee634ebb8af3f4c5062607c22786c1
Mukta06 <cbmukta06@gmail.com>
2024-08-26 13:08:08 +0530

    Auto push

--- tf-module-vpc/sg.tf
+++ tf-module-vpc/sg.tf
@@ -3,14 +3,14 @@ resource "aws_security_group" "alb-sg" {
     description = "Security Group for Application Load Balancer"
     vpc_id = aws_vpc.Mukta.id
 
-    ingress = {
+    ingress {
         from_port        = 80
         to_port          = 80
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
     }
 
-    egress = {
+    egress {
         from_port        = 0
         to_port          = 0
         protocol         = "-1"
