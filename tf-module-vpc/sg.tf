0aaf3f1a2579dca4da04ebf29e8f6bf7fa3526e0
Mukta06 <cbmukta06@gmail.com>
2024-08-26 14:26:59 +0530

    Auto push

--- tf-module-vpc/sg.tf
+++ tf-module-vpc/sg.tf
@@ -30,9 +30,9 @@ resource "aws_security_group" "ec2-sg" {
     vpc_id = aws_vpc.Mukta.id
 
     ingress {
-        from_port            = 80
-        to_port              = 80
-        protocol             = "tcp"
+        from_port            = 0
+        to_port              = 0
+        protocol             = "-1"
         security_groups      = [aws_security_group.alb-sg.id]
     }
 
--- tf-module-vpc/vpc.tf
+++ tf-module-vpc/vpc.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc" "Mukta" {
 
     cidr_block              = var.VPC_CIDR
-    instance_tenancy        = "default"
+    #instance_tenancy        = "default"
     enable_dns_hostnames    = true
     tags = {
       Name="Mukta-VPC"
