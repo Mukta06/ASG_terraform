c8e77305efac4753a741f280fe94137b520d5ff1
Mukta06 <cbmukta06@gmail.com>
2024-08-26 15:10:47 +0530

    Auto push

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ASG_terraform
+Terraform code to launch VPC with 2 Subnets and Auto Scaling groups to increase no of machines to 5
+
--- tf-module-vpc/sg.tf
+++ tf-module-vpc/sg.tf
@@ -30,11 +30,11 @@ resource "aws_security_group" "ec2-sg" {
     vpc_id = aws_vpc.Mukta.id
 
     ingress {
-        from_port            = 80
-        to_port              = 80
-        protocol             = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
-        #security_groups      = [aws_security_group.alb-sg.id]
+        from_port            = 0
+        to_port              = 0
+        protocol             = "-1"
+        #cidr_blocks      = ["0.0.0.0/0"]
+        security_groups      = [aws_security_group.alb-sg.id]
     }
 
     egress {
