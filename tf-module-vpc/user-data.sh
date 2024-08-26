60bc924b984e20797802ea10cd192451bca59f5b
Mukta06 <cbmukta06@gmail.com>
2024-08-26 14:04:48 +0530

    Auto push

--- tf-module-vpc/user-data.sh
+++ tf-module-vpc/user-data.sh
@@ -1,1 +1,7 @@
-# In this You can place the code to deploy
+#!/bin/bash
+
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>The Server IP is : $(hostname -i)</h1> " > /var/www/html/index.html
